7566341df92d2a0f2eb4ac29c0de0f2845da2200
SHJ <88483648+Enniasoh@users.noreply.github.com>
2021-11-18 14:51:58 +0800

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -19,3 +19,8 @@ with(subset_data, {
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+##save to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+
+##to close off
+dev.off()
